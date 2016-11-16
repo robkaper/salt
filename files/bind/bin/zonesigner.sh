057da5f317d508911998d158924f380623b63ebe
Rob Kaper <rob@robkaper.nl>
2016-11-16 16:18:09 +0100

    don't sign unsigned zones automatically

--- files/bind/bin/zonesigner.sh
+++ files/bind/bin/zonesigner.sh
@@ -10,13 +10,17 @@ for ZONEFILE in $(ls ${ZONEDIR}/*.db); do
 	ZONE=$(basename ${ZONEFILE} | sed s/\.db//)
 	echo "${ZONE}:"
 	if [ ! -f ${ZONEFILE}.signed ]; then
-		echo "Unsigned, --genkeys"
-		zonesigner --genkeys -zone ${ZONE} ${ZONEFILE} 2>/dev/null
+		echo "Unsigned, run manually:"
+		echo "zonesigner --genkeys -zone ${ZONE} ${ZONEFILE}"
+		echo "And add DNSSEC key to WHOIS"
+		exit 1
+		# zonesigner --genkeys -zone ${ZONE} ${ZONEFILE} 2>/dev/null
   elif [ ${ZONEFILE}.signed -ot ${ZONEFILE} ]; then
 		echo "Signed, update for changes"
 		zonesigner -zone ${ZONE} ${ZONEFILE} 2>/dev/null
+		touch ${ZONEFILE}
 	else
-		SIGDATE=$(date +%s -r ${ZONEFILE})
+		SIGDATE=$(date +%s -r ${ZONEFILE}.signed)
 		if [ $(( ${NOW} - ${SIGDATE} )) -gt $(( 86400*14 )) ]; then
       echo "Signed, update (signature older than 14d)"
       zonesigner -zone ${ZONE} ${ZONEFILE} 2>/dev/null
